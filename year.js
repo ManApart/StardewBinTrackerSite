8304e64beea30dd0c576b576591e1b3197cc0876
ManApart <raustinkucera@gmail.com>
2019-12-21 20:05:46 -0500

    cleanup

--- year.js
+++
@@ -1,6 +0,0 @@
-window.onload = function () {
-    const data = JSON.parse(this.localStorage.getItem("CropHistory"))
-    console.log('data', this.cropHistory)
-    // console.log('data', JSON.stringify(data))
-
-}
