6cf944c914332110a10694b902f9e173482f55a6
ManApart <raustinkucera@gmail.com>
2019-12-22 13:49:05 -0500

    breaking rename to fix path

--- old/AKApp.js
+++
@@ -1,6 +0,0 @@
-(function(){
-	var app = angular.module('akMain', ['ngAnimate','chartController','mainController','cropHistoryService']);
-		app.controller('akMainController', function($scope){	 
-		     
-	});
-})();
